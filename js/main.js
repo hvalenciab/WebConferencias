ca36234b69bad01b168fa61ced5c88de31434b11
hvalenciab <63340725+hvalenciab@users.noreply.github.com>
2020-06-06 01:36:51 -0500

    Actualizados

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
     "use strict";
-    
+
     var regalo = document.getElementById('regalo');
     document.addEventListener('DOMContentLoaded', function() {
 
@@ -78,15 +78,47 @@
         }
     }
 
-
     var select = document.getElementById('regalo');
-select.addEventListener('change',
-  function(){
+    select.addEventListener('change', function(){
     var selectedOption = this.options[select.selectedIndex];
     turegalo.style.display = "block";
     turegalo.innerHTML = ('Regalo' + ': ' + selectedOption.text);
-    console.log(selectedOption.value + ': ' + selectedOption.text);
   });
+
 
 }); // DOM content Loaded
 })();
+
+
+$(function() {
+    
+    // Programa de conferencias
+     $('.programa-evento .info-curso:first').show();
+     $('.menu-programa a:first').addClass('activo');
+    
+     $('.menu-programa a').on('click', function() {
+         $('.menu-programa a').removeClass('activo');
+         $(this).addClass('activo');
+         $('.ocultar').hide();
+         var enlace = $(this).attr('href');
+         $(enlace).fadeIn(1000);
+         return false;
+     });
+
+
+    //Animaciones
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1500);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1600);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1800);
+
+    //Contador de dias
+    $('.cuenta-regresiva').countdown('2020/12/11 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+
+    });
+
+});
